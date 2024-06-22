beff355539598de2be5421960f015d633c370d4b
SakiBee <hi.sakib75@gmail.com>
2024-06-22 12:55:17 +0600

    add files

---
+++ src/components/AddTodo.js
@@ -0,0 +1,33 @@
+import React, { useState, useContext } from "react";
+import { TodoContext } from "../context/TodoContext";
+import "./AddTodo.css";
+
+const AddTodo = () => {
+  const { addTodoAction, state } = useContext(TodoContext);
+  const [title, setTitle] = useState("");
+
+  const handleTitleChange = e => {
+    setTitle(e.target.value);
+  };
+
+  //submit
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (title === "") {
+      return alert("please Provide all details");
+    }
+    addTodoAction(title);
+    setTitle("");
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input value={title} onChange={handleTitleChange} />
+        <button type="submit">Add Todo</button>
+      </form>
+    </>
+  );
+};
+
+export default AddTodo;
---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { TodoContext } from "../context/TodoContext";
+import "./TodoList.css";
+
+const TodoList = () => {
+  const { todos, removeTodoAction } = useContext(TodoContext);
+  return (
+    <div className="todo-list-container">
+      <div className="todo-item-container">
+        {todos.map(todo => {
+          return (
+            <div key={todo.id} className="todo-item-container">
+              <h3>{todo.title}</h3>
+              <button onClick={() => removeTodoAction(todo.id)}>Delete</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
